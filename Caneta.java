b193055619bff00c34c074aa40498cfa2f168707
iasmimaraujol <iasmim.engenharia@gmail.com>
2022-06-06 11:27:03 -0300

    adicionando os modificadores de acesso

--- Caneta.java
+++ Caneta.java
@@ -16,16 +16,27 @@ public class Caneta {
     protected int carga;
     protected boolean tampada;
     //construtor
-    public void modCaneta(String modelo, String cor, float ponta){
-        this.modelo = modelo;
-        this.cor = cor;
-        this.ponta = ponta;
+    public Caneta(String m, String c, float p){
+        this.modelo = m;
+        this.cor = c;
+        this.ponta = p;
         this.carga = 100;
         this.tampada = true;
-
     }
 
     //modificadores
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public float getPonta(){
+        return this.ponta;
+    }
+    public void setPonta(float ponta){
+        this.ponta = ponta;
+    }
 
     //metodos
     public void rabiscar(){
--- teste.java
+++ teste.java
@@ -10,22 +10,10 @@
 class teste{
     public static void main(String [] args){
         //System.out.println("Hello project");
-        Caneta c1 = new Caneta();
-        c1.tampada = false;
-        c1.carga = 90;
-        c1.cor = "azul";
-        c1.modelo = "bic";
-
-        Caneta c2 = new Caneta();
-        c2.tampada = true;
-        c2.carga = 90;
-        c2.cor = "vermelha";
-        c2.modelo = "bic";
-
-        c1.rabiscar();
+        Caneta c1 = new Caneta("Bic", "azul", 0.5f);
+        Caneta c2 = new Caneta("Castle", "red", 1.5f);
+        
         c1.printOn();
-
-        c2.rabiscar();
         c2.printOn();
         
         }    
